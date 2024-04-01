8f8a6f177a0404d61a76b2faf427e5a990735edd
chodot <89974193+chodott@users.noreply.github.com>
2024-04-01 13:18:25 +0900

    백준 집합의 표현 문제 해결 (1717번)
    골드 5단계
    union_set

---
+++ 1717.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <vector>
+using namespace std;
+
+int findRoot(vector<int>& union_set, int a)
+{
+	if (union_set[a] == a)
+		return a;
+	else return union_set[a] = findRoot(union_set, union_set[a]);
+}
+void merge(vector<int>&union_set, int a, int b)
+{
+	int a_root, b_root;
+	a_root = findRoot(union_set, a);
+	b_root = findRoot(union_set, b);
+	if (a_root == b_root) return;
+	union_set[b_root] = a_root;
+}
+
+int main()
+{
+	int n, m;
+	int cmd, a, b;
+	scanf_s("%d %d", &n, &m);
+	vector<int> union_set;
+	for (int i = 0; i <= n; ++i) union_set.emplace_back(i);
+	for (int i = 0; i < m; ++i)
+	{
+		scanf_s("%d %d %d", &cmd, &a,&b);
+		switch (cmd)
+		{
+		case 0:
+			merge(union_set, a, b);
+			break;
+		case 1:
+			if (findRoot(union_set, a) == findRoot(union_set, b)) printf("yes\n");
+			else printf("no\n");
+			break;
+		}
+	}
+
+}
