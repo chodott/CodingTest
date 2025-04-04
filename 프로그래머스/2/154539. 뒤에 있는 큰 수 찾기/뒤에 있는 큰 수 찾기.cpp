670845443f7c3eed25e5243481b4b113c7b85690
chodot <89974193+chodott@users.noreply.github.com>
2025-04-04 12:38:22 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 335.74 ms, Memory: 240 MB -BaekjoonHub

--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.cpp
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.cpp
@@ -7,26 +7,22 @@ using namespace std;
 
 vector<int> solution(vector<int> numbers) {
     int length = numbers.size();
-    vector<int> answer;
+    vector<int> answer(length,-1);
     stack<int> num_stack;
     for(int i=length-1; i>=0; --i)
     {
         int num = numbers[i];    
-        bool bSuccess = false;
         while(!num_stack.empty())
         {
             int value = num_stack.top();
             if(num < value)
             {
-                answer.push_back(value);
-                bSuccess = true;
+                answer[i] = value;
                 break;
             }
             else num_stack.pop();
         }     
         num_stack.push(num);
-        if(!bSuccess) answer.push_back(-1);
     }
-    reverse(answer.begin(), answer.end());
     return answer;
 }
