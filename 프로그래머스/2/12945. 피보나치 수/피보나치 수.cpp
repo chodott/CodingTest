5ae363fdce3253fd7f0183613f14e4f39599e932
chodot <89974193+chodott@users.noreply.github.com>
2025-05-27 20:49:31 +0900

    [level 2] Title: 피보나치 수, Time: 3047.21 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    int dp[100000] = {0,1,1};
+    for(int i=2;i<=n;++i) dp[i] = (dp[i-1] + dp[i-2]) % 1234567;
+    return dp[n];
+}
