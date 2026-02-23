21243ce5d8a13b73a1df46a4e4220206f6193604
chodot <chohs0514@naver.com>
2026-02-24 00:33:21 +0900

    [Silver III] Title: 수열, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.cc
+++ 백준/Silver/2559. 수열/수열.cc
@@ -6,6 +6,7 @@ using namespace std;
 
 int main()
 {
+	ios_base::sync_with_stdio(false);
 	int n, k; cin >> n >> k;
 	vector<int> arr(n, 0);
 	for (int i = 0; i < n; ++i)
@@ -14,22 +15,14 @@ int main()
 	}
 
 	int leftSum = accumulate(arr.begin(), arr.begin() + k, 0);
-	int rightSum = accumulate(arr.end() - k, arr.end(), 0);
 	int leftAnswer = leftSum;
-	int rightAnswer = rightSum;
 	pair<int, int> leftPos = { 0, k - 1 };
-	pair<int, int> rightPos = { n - k, n - 1 };
 
-	for (int i = 0; i < (n - k)/2; ++i)
+	for (int i = 0; i < n - k; ++i)
 	{
 		leftSum -= arr[leftPos.first++];
 		leftSum += arr[++leftPos.second];
-
-		rightSum -= arr[rightPos.second--];
-		rightSum += arr[--rightPos.first];
-
 		leftAnswer = max(leftAnswer, leftSum);
-		rightAnswer = max(rightAnswer, rightSum);
 	}
-	cout << max(leftAnswer, rightAnswer);
+	cout << leftAnswer;
 }
